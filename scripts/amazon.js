25b0602a25cc6770d93171771a1ef0d703cf3123
gaug1801 <gaug1801@gmail.com>
2024-10-12 14:39:08 -0500

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -15,6 +15,21 @@
   Thus, we save each product as an object.
   Below, this array of objects is called DATA STRUCTURE.
     We usually use a combination of objects and arrays to create data structures.
+
+  Data Attribute:
+    Used to pass information to from JavaScript to the HTML.
+    To use it, we create an attribute that starts with 'data-',
+    then any name can be assigned to it. Ex. data-product-name.
+    It HAS to start with data- and must be in kebab-case.
+    In this case, when generating the HTML, we pass into it
+    the name of the product.
+      - DOM Notes: Element.dataset returns ALL data attributes associated
+      with the selected HTML element as an array of said objects
+        - the data attribute name is converted from kebab-case to camelCase
+        at this point.
+        - Thus, to access the data, we use 'Element.dataset.objectName'
+        - Ex. button.dataset.productName
+
 */
 
 let productsHTML = '';
@@ -65,7 +80,8 @@ products.forEach( (product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart" 
+      data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
@@ -76,3 +92,36 @@ console.log(productsHTML);
 
 document.querySelector('.products-grid')
 .innerHTML = productsHTML;
+
+/*
+  Steps taken:
+    1. pass the product name to the add to cart button
+    2. retrieve the name with dataset
+    3. add it to the cart + the quantity
+*/
+
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button) => {
+  button.addEventListener('click', ()=> {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item)=> {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    //I hadn't given thought to checking if it exists. This is a good way to resolve
+    if (matchingItem) {
+      matchingItem.quantity++;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }
+
+    console.log(cart);
+  });
+});
