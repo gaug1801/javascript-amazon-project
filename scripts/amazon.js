3fd35d20178ce25166e8f796efe572b3c513f7ab
gaug1801 <gaug1801@gmail.com>
2024-12-27 12:10:06 -0600

    Load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -64,8 +64,11 @@
 */
 
 import { addToCart,calculateCartQuantity } from '../data/cart.js';
-import { products } from '../data/products.js';
+import { products, loadProducts } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+
+
+loadProducts(renderProductsGrid);
 
 /*
   Update cart quantity upon loading the web page.
@@ -77,85 +80,87 @@ document.querySelector('.js-cart-quantity').innerHTML = calculateCartQuantity();
   Generate HTML using accumulator pattern.
 */
 
-let productsHTML = '';
+function renderProductsGrid() {
+  let productsHTML = '';
 
-products.forEach( (product) => {
-  productsHTML += `
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
-      </div>
-
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars"
-          src="${product.getStarsUrl()}">
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
+  products.forEach( (product) => {
+    productsHTML += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image"
+            src="${product.image}">
         </div>
+
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars"
+            src="${product.getStarsUrl()}">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
+
+        <div class="product-price">
+          ${product.getPrice()}
+        </div>
+
+        <div class="product-quantity-container">
+          <select>
+            <option selected value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+        </div>
+
+        ${product.extraInfoHTML()}
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary js-add-to-cart" 
+        data-product-id="${product.id}">
+          Add to Cart
+        </button>
       </div>
-
-      <div class="product-price">
-        ${product.getPrice()}
-      </div>
-
-      <div class="product-quantity-container">
-        <select>
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-
-      ${product.extraInfoHTML()}
-
-      <div class="product-spacer"></div>
-
-      <div class="added-to-cart">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
-
-      <button class="add-to-cart-button button-primary js-add-to-cart" 
-      data-product-id="${product.id}">
-        Add to Cart
-      </button>
-    </div>
-  `;
-});
-
-
-document.querySelector('.products-grid')
-.innerHTML = productsHTML;
-
-/*
-  Event Listener for adding an item to the cart.
-
-  1. Pass the product name to the "Add to Cart" button.
-  2. Retrieve the name using dataset.
-  3. Add it to the cart .
-  4. Calculate new cart quantity.
-  5. Pass new cart quantity to HTML.
-*/
-
-document.querySelectorAll('.js-add-to-cart')
-.forEach((button) => {
-  button.addEventListener('click', ()=> {
-    const productId = button.dataset.productId;
-    
-    addToCart(productId);
-    
-    document.querySelector('.js-cart-quantity').innerHTML = calculateCartQuantity();
+    `;
   });
-});
+
+
+  document.querySelector('.products-grid')
+  .innerHTML = productsHTML;
+
+  /*
+    Event Listener for adding an item to the cart.
+
+    1. Pass the product name to the "Add to Cart" button.
+    2. Retrieve the name using dataset.
+    3. Add it to the cart .
+    4. Calculate new cart quantity.
+    5. Pass new cart quantity to HTML.
+  */
+
+  document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', ()=> {
+      const productId = button.dataset.productId;
+      
+      addToCart(productId);
+      
+      document.querySelector('.js-cart-quantity').innerHTML = calculateCartQuantity();
+    });
+  });
+}
