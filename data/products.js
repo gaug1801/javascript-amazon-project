4717979faba1c2b15bcef472e61449b4af24f586
gaug1801 <gaug1801@gmail.com>
2024-12-28 12:15:37 -0600

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -122,6 +122,8 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise; //  6. returning a response allows us to attach another .then() wherever the function is called.
@@ -137,14 +139,23 @@ export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
   xhr.addEventListener('load', ()=> {
-    
+    products = JSON.parse(xhr.response).map((productDetails)=> { 
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      } else if(productDetails.type === 'appliance') {
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
   });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  })
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
-
-
 
 /*
 export const products = [
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,14 +4,22 @@ import { loadProducts, loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js'
 
 async function loadPage() {
+  try {
+    //throw 'error1';
 
-  await loadProductsFetch();
+    await loadProductsFetch();
 
-  const value = await new Promise((resolve)=> {
-    loadCart(()=> {
-      resolve();
+    const value = await new Promise((resolve, reject)=> {
+      //throw 'error2';
+      loadCart(()=> {
+        // reject('error3')
+        resolve('value3');
+      });
     });
-  });
+
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+  }
 
   renderOrderSummary();
   renderPaymentSummary();
